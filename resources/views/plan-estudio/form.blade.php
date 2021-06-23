1278a6035aa5e0a89a917b63ad351c30dc170483
Rebjai <rebjai@github.com>
2021-06-23 02:28:31 -0500

    button fixes

--- resources/views/plan-estudio/form.blade.php
+++ resources/views/plan-estudio/form.blade.php
@@ -1,19 +1,20 @@
 <div class="box box-info padding-1">
     <div class="box-body">
-        
+
         <div class="form-group">
             {{ Form::label('nombre') }}
             {{ Form::text('nombre', $planEstudio->nombre, ['class' => 'form-control' . ($errors->has('nombre') ? ' is-invalid' : ''), 'placeholder' => 'Nombre']) }}
-            {!! $errors->first('nombre', '<div class="invalid-feedback">:message</p>') !!}
+            {!! $errors->first('nombre', '<div class="invalid-feedback">:message</div>') !!}
         </div>
         <div class="form-group">
             {{ Form::label('clave') }}
             {{ Form::text('clave', $planEstudio->clave, ['class' => 'form-control' . ($errors->has('clave') ? ' is-invalid' : ''), 'placeholder' => 'Clave']) }}
-            {!! $errors->first('clave', '<div class="invalid-feedback">:message</p>') !!}
+            {!! $errors->first('clave', '<div class="invalid-feedback">:message</div>') !!}
         </div>
 
     </div>
     <div class="box-footer mt20">
-        <button type="submit" class="btn btn-primary">Submit</button>
+        <a class="btn btn-danger" href="{{route('plan-estudios.index')}}">Cancelar</a>
+        <button type="submit" class="btn btn-primary">Continuar</button>
     </div>
 </div>
