204ad6aa96875ae842d00c0f0f1f066471642829
Rebjai <rebjai@github.com>
2021-06-25 15:00:39 -0500

    liberaciones sql

--- database/migrations/2021_06_23_090731_create_liberacions_table.php
+++ database/migrations/2021_06_23_090731_create_liberacions_table.php
@@ -16,18 +16,34 @@ class CreateLiberacionsTable extends Migration
         Schema::create('liberacions', function (Blueprint $table) {
             $table->id();
             $table->string('fecha');
-
+            
             $table->unsignedBigInteger('docente_id');
-
+            
             $table->foreign('docente_id')
-                ->references('id')->on('docentes');
-
+            ->references('id')->on('docentes')->onDelete('cascade');
+            
             $table->string('semestre');
-
+            
             $table->timestamps();
         });
-    }
+        Schema::create('actividades', function (Blueprint $table) {
+            $table->id();
+            $table->string('descripcion');
+        });
+        Schema::create('reporte_actividades', function (Blueprint $table) {
+            $table->unsignedBigInteger('liberacion_id');
+            $table->unsignedBigInteger('actividad_id');
+            $table->integer('evaluacion');
 
+            $table->foreign('liberacion_id')
+            ->references('id')->on('liberacions')->onDelete('cascade');
+            $table->foreign('actividad_id')
+            ->references('id')->on('actividades')->onDelete('restrict');
+            
+        });
+        
+    }
+    
     /**
      * Reverse the migrations.
      *
@@ -36,5 +52,7 @@ class CreateLiberacionsTable extends Migration
     public function down()
     {
         Schema::dropIfExists('liberacions');
+        Schema::dropIfExists('actividades');
+        Schema::dropIfExists('reporte_actividades');
     }
 }
