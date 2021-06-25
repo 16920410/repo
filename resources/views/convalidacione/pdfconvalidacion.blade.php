2276c5f3e57f92b565976fc5100b8b15e351baa7
Rebjai <rebjai@github.com>
2021-06-25 12:52:14 -0500

    fix pdf rendering, tr tag missing

--- resources/views/convalidacione/pdfconvalidacion.blade.php
+++ resources/views/convalidacione/pdfconvalidacion.blade.php
@@ -1,59 +1,74 @@
 <html>
 
 
-<style>
-    body {
-        width: 100%;
-    }
 
-    table {
-        width: 80%;
-        margin: 0 auto;
-        border: 1px solid black;
-        border-collapse: collapse;
-    }
-
-    th,
-    td {
-        border: 1px solid black;
-    }
-
-    .estudiante {
-        width: 80%;
-        margin: 0 auto;
-    }
-</style>
 
 <body>
-    <div class="info">
-        <div class="estudiante">Nombre del estudiante: {{$convalidacion->nombre_alumno}}</div>
-        <div class="div">
-            <table>
-                <thead>
-                    <th>De</th>
-                    <th>A</th>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>Nombre del Plan de Estudios: {{$convalidacion->plan_externo}}</td>
-                        <td>Nombre del Plan de Estudios: {{$convalidacion->plan_interno}}</td>
-                    </tr>
-                    <tr>
-                        <td>Clave del Plan de Estudios: {{$convalidacion->plan_externo_clave}}</td>
-                        <td>Clave del Plan de Estudios: {{$convalidacion->plan_interno_clave}}</td>
-                    </tr>
-                    <tr>
-                        <td>Institución de procedencia: {{$convalidacion->tecnologico_procedente}}</td>
-                        <td>Institución receptora: {{$convalidacion->tecnologico_receptor}}</td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
-    </div>
+    <style>
+        body {
+            width: 100%;
+        }
+
+        table {
+            width: 80%;
+            margin: 0 auto;
+            border: 1px solid black;
+            border-collapse: collapse;
+        }
+
+        thead,
+        tbody {
+            width: 100%;
+        }
+
+        th,
+        td {
+            width: 100%;
+            border: 1px solid black;
+            border-collapse: collapse;
+        }
+
+        .estudiante {
+            width: 80%;
+            margin: 0 auto;
+        }
+    </style>
+    <!-- <div class="info"> -->
+    <div class="estudiante">Nombre del estudiante: {{$convalidacion->nombre_alumno}}</div>
+    <!-- <div class="div"> -->
+    <table>
+        <thead>
+            <tr>
+                <th>De</th>
+                <th>A</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Nombre del Plan de Estudios: {{$convalidacion->plan_externo}}</td>
+                <td>Nombre del Plan de Estudios: {{$convalidacion->plan_interno}}</td>
+            </tr>
+            <tr>
+                <td>Clave del Plan de Estudios: {{$convalidacion->plan_externo_clave}}</td>
+                <td>Clave del Plan de Estudios: {{$convalidacion->plan_interno_clave}}</td>
+            </tr>
+            <tr>
+                <td>Institución de procedencia: {{$convalidacion->tecnologico_procedente}}</td>
+                <td>Institución receptora: {{$convalidacion->tecnologico_receptor}}</td>
+            </tr>
+            <tr>
+                <td>Institución de procedencia: {{$convalidacion->tecnologico_procedente}}</td>
+                <td>Institución receptora: {{$convalidacion->tecnologico_receptor}}</td>
+            </tr>
+        </tbody>
+    </table>
+    <!-- </div> -->
+    <!-- </div> -->
     <br><br><br>
-    <div class="tabla-conv">
-        <table>
-            <thead>
+    <!-- <div class="tabla-conv"> -->
+    <table>
+        <thead>
+            <tr>
                 <th>No.</th>
                 <th>Asignatura cursada</th>
                 <th>Clave de asignatura</th>
@@ -61,25 +76,26 @@
                 <th>Asignatura a convalidar</th>
                 <th>Clave de asignatura a convalidar</th>
                 <th> Porcentaje %</th>
-            </thead>
-            <tbody>
-                @foreach ($materias_convalidadas as $key => $materia)
-                <tr>
-                    <td>{{$key+1}}</td>
-                    <td>{{$materia->cursada}}</td>
-                    <td>{{$materia->cursada_clave}}</td>
-                    <td>{{$materia->calificacion}}</td>
-                    <td>{{$materia->convalidada}}</td>
-                    <td>{{$materia->convalidada_clave}}</td>
-                    <td>{{$materia->porcentaje}}</td>
-                </tr>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($materias_convalidadas as $key => $materia)
+            <tr>
+                <td>{{$key+1}}</td>
+                <td>{{$materia->cursada}}</td>
+                <td>{{$materia->cursada_clave}}</td>
+                <td>{{$materia->calificacion}}</td>
+                <td>{{$materia->convalidada}}</td>
+                <td>{{$materia->convalidada_clave}}</td>
+                <td>{{$materia->porcentaje}}</td>
+            </tr>
 
-                @endforeach
+            @endforeach
 
 
-            </tbody>
-        </table>
-    </div>
+        </tbody>
+    </table>
+    <!-- </div> -->
 </body>
 
 </html>
